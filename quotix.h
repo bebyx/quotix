90b23983c7e28dfa3e4ac076ed758e6fc78d651c
bebyx <bebyx@protonmail.com>
2021-04-02 22:52:21 +0300

    Add include guards

--- quotix.h
+++ quotix.h
@@ -1,3 +1,8 @@
+#ifndef QUOTIX_H_INCLUDED
+#define QUOTIX_H_INCLUDED
+
 void check_file(const char* filename);
 int count_lines(const char* filename);
 void print_random_quote(const char* filename, int lines_count);
+
+#endif
