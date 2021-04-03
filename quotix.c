26d71e317d29c61dd1a12659e84088819ae4673e
bebyx <bebyx@protonmail.com>
2021-04-03 17:34:25 +0300

    Update code style

--- quotix.c
+++ quotix.c
@@ -5,22 +5,27 @@
 #include <string.h>
 #include "quotix.h"
 
-void check_file(const char* filename) {
+void check_file(const char* filename)
+{
   FILE *fptr;
-  if ((fptr = fopen(filename, "r")) == NULL) {
+  if ((fptr = fopen(filename, "r")) == NULL)
+  {
     fprintf(stderr, "File error %d: %s — '%s'\n", errno, strerror(errno), filename);
     exit(EXIT_FAILURE);
-  } else {
+  }
+  else
+  {
     fclose(fptr);
   }
-
 }
 
-int count_lines(const char* filename) {
+int count_lines(const char* filename)
+{
   FILE *fptr;
   fptr = fopen(filename, "r");
   char c; int count = 0;
-  while ((c = fgetc(fptr)) != EOF) {
+  while ((c = fgetc(fptr)) != EOF)
+  {
    if (c == '\n')
       count++;
   }
@@ -41,20 +46,24 @@ void seed_random(const char* iteration)
     srand(time(0)/60/60/24);
 }
 
-void print_random_quote(const char* filename, int lines_count) {
+void print_random_quote(const char* filename, int lines_count)
+{
   FILE *fptr;
   fptr = fopen(filename, "r");
   int count = 0, line_number;
   char buffer[256];
 
   line_number = rand() % lines_count;
-  while (fgets(buffer, sizeof buffer, fptr) != NULL) {
-    if (count == line_number) {
+  while (fgets(buffer, sizeof buffer, fptr) != NULL)
+  {
+    if (count == line_number)
+    {
       fprintf(stdout, "%s", buffer);
       fclose(fptr);
       exit(0);
     }
-    else {
+    else
+    {
       count++;
     }
   }
