ecef452aeee4a31566017419185f53fe92538aee
bebyx <artem@bebyx.co.ua>
2021-12-10 02:06:42 +0200

    Squashed commit of the following:
    commit a19cb71538e9b5c405fe3cb57f879c8897aca47a
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Fri Dec 10 01:58:45 2021 +0200
    
        Reflect changes in README
    
    commit e570544eecb21060d57452aff983a5e3b0ddcd17
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Fri Dec 10 01:45:29 2021 +0200
    
        Avoid sed src code by using gcc -D flag to define path of quotes list
    
    commit e90f93107df0ba096bdf3585e99ba2a92f893ca7
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Fri Dec 10 01:17:17 2021 +0200
    
        Fix malloc unpredictability with critical NULL terminator
    
    commit 9cb2a958f275d0ce07c0c35a430b51af4b6276ff
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 21:51:04 2021 +0200
    
        Fix bug with 'no quotes avaialble' when they are
    
    commit 2532031f7660145fdd739f885ebdefcfaabc3f83
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 20:38:34 2021 +0200
    
        Error exit upon wrong flag
    
    commit a4ca68e8cafba6a647718ad5115a1df3947497f0
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 20:30:00 2021 +0200
    
        Use getopt.h instead of bicycle; minor clean-up
    
    commit 000987aba5fc6f318d0c84fd947eeb3560641246
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 16:43:25 2021 +0200
    
        Refactor quotix module
    
    commit 1fee9bc70158b544ff2196fa38d7470b4ed72cb0
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 15:23:33 2021 +0200
    
        Update assets identifier
    
    commit fd4c54d53d8fa9274f6d76bb18fd7c3a6049faa1
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 14:46:17 2021 +0200
    
        Clean up, prettify, add comments
    
    commit a8673a991c9bb83817c223150fa6d0f66e13127b
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 13:36:42 2021 +0200
    
        Check if file contains available lines to display
    
    commit 5bf5739cf274ddbdbaa0c0dd84bae1c99e6cb484
    Author: bebyx <artem@bebyx.co.ua>
    Date:   Thu Dec 9 04:03:31 2021 +0200
    
        Barebones of comment-out feature

---
+++ src/core.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "core.h"
+#include "checks.h"
+
+int count_lines(const char* filename)
+{
+  FILE *stream;
+  char c; int line_count = 0, char_count = 0;
+
+  // Count all lines
+  stream = filter_material(filename);
+  while ((c = fgetc(stream)) != EOF)
+  {
+   if (c == '\n')
+   {
+      line_count++;
+      check_line(line_count, char_count);
+      char_count = 0;
+   }
+   else
+   {
+     char_count++;
+   }
+  }
+
+  fclose(stream);
+
+  return line_count;
+}
+
+void seed_random(const char* iteration)
+{
+  if (strcmp(iteration, "second") == 0)
+    srand(time(0));
+  else if (strcmp(iteration, "minute") == 0)
+    srand(time(0)/60);
+  else if (strcmp(iteration, "hour") == 0)
+    srand(time(0)/60/60);
+  else if (strcmp(iteration, "hour") == 0)
+    srand(time(0)/60/60/24);
+}
+
+void print_random_quote(const char* filename)
+{
+  int count = 0, line_number;
+  char buffer[LINE_LIMIT+1] = "";
+  FILE *stream;
+
+  int lines_quantity = count_lines(filename);
+  check_content(lines_quantity);
+
+  line_number = rand() % lines_quantity;
+
+  stream = filter_material(filename);
+
+  // Print out random line
+  while (fgets(buffer, sizeof(buffer), stream) != NULL)
+  {
+    if (count == line_number)
+    {
+      fprintf(stdout, "%s", buffer);
+      fclose(stream);
+      exit(0);
+    }
+    else
+    {
+      count++;
+    }
+  }
+
+  fclose(stream);
+}
+
+FILE *filter_material(const char* filename)
+{
+  FILE *fptr;
+  char buffer[LINE_LIMIT+1] = "";
+  FILE *stream;
+
+  // Calculate size of the file
+  fptr = fopen(filename, "r");
+  fseek(fptr, 0L, SEEK_END);
+  long int file_size = ftell(fptr);
+  fclose(fptr);
+
+  // Initialize variable-sized string
+  char *filtered_material = (char *) malloc(file_size);
+  filtered_material[0] = '\0';
+
+  // Filter out commented lines
+  fptr = fopen(filename, "r");
+  while (fgets(buffer, sizeof(buffer), fptr) != NULL)
+  {
+    if (buffer[0] == '#')
+    {
+      continue;
+    }
+    strcat(filtered_material, buffer);
+  }
+  fclose(fptr);
+
+  // Convert string to stream
+  stream = fmemopen(filtered_material, strlen(filtered_material), "r");
+
+  return stream;
+}
